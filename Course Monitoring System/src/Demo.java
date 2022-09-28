8f85cf925e4ea8486b4386e2f45e99ae63530a36
Arup Mahato <arup71062@gmail.com>
2022-09-28 13:42:18 +0530

    utility package

--- Course Monitoring System/src/Demo.java
+++ Course Monitoring System/src/Demo.java
@@ -1,5 +1,12 @@
+import utility.Dbc;
+
+import java.sql.Connection;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("This is for checking ");
+
+        Connection conn= Dbc.getConnection();
+        System.out.println(conn);
     }
 }
---
+++ Course Monitoring System/src/utility/Dbc.java
@@ -0,0 +1,29 @@
+package utility;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbc {
+    public static Connection getConnection() {
+        Connection c = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/cms";
+
+        try {
+            c = DriverManager.getConnection(url, "root", "Arup@MySQL");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return c;
+    }
+
+}
+
